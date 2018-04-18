1498029f6dafaed149ec06c44515c9f7f2441e49
Kevin Buffardi <kbuffardi@csuchico.edu>
2018-04-17 21:37:59 -0700

    Starter

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//prototypes:
+
+
+/////DO NOT TOUCH/////
+int main()
+{
+  vector <int> projects;
+  vector <int> quizzes;
+
+  int numProjects;
+  int numQuizzes;
+
+  cout<<"\nHow many projects are there? ";
+  cin>>numProjects;
+  cout<<"\nHow many quizzes are there? ";
+  cin>>numQuizzes;
+
+  //only proceed if both numbers are positive
+  if( isPositive(numProjects) && isPositive(numQuizzes) )
+  {
+    projects.resize(numProjects);
+    quizzes.resize(numQuizzes);
+
+    fillWith(projects, 90); // Fills the vector with all value 90s
+    fillWith(quizzes, 80); // Fills the vector with all value 80s
+
+    cout<<"\nProject Scores:\n";
+    printVector(projects); // Prints vector with space after each cell
+    cout<<"\n\nQuiz Scores:\n";
+    printVector(quizzes); // Prints vector with space after each cell
+  }
+  else
+  {
+    cout<<"\nSorry, numbers must be bigger than 0.\n";
+  }
+  return 0;
+}
+
+//function definitions
